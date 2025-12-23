9b3da67bca54a8c9b50983dcf8596906b22ee28a
Aesha Mangukiya <aesha.m@bluebittechnologies.com>
2025-12-23 12:27:16 +0530

    Update env configuration

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,20 +1,46 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-export const ENV = {
-  BASE_URL: process.env.BASE_URL!,
-  USERS: {
-    ADMIN: {
-      username: process.env.ADMIN_USERNAME!,
-      password: process.env.ADMIN_PASSWORD!
-    },
-    USER: {
-      username: process.env.USER_USERNAME!,
-      password: process.env.USER_PASSWORD!
-    },
-    SUPPORT: {
-      username: process.env.SUPPORT_USERNAME!,
-      password: process.env.SUPPORT_PASSWORD!
+type EnvironmentType = 'staging' | 'live';
+
+const CURRENT_ENV = (process.env.ENVIRONMENT as EnvironmentType) || 'live';
+
+const ENV_CONFIG = {
+  staging: {
+    BASE_URL: process.env.STAGING_BASE_URL!,
+    USERS: {
+      ADMIN: {
+        username: process.env.STAGING_ADMIN_USERNAME!,
+        password: process.env.STAGING_ADMIN_PASSWORD!
+      },
+      USER: {
+        username: process.env.STAGING_USER_USERNAME!,
+        password: process.env.STAGING_USER_PASSWORD!
+      },
+      SUPPORT: {
+        username: process.env.STAGING_SUPPORT_USERNAME!,
+        password: process.env.STAGING_SUPPORT_PASSWORD!
+      }
+    }
+  },
+
+  live: {
+    BASE_URL: process.env.LIVE_BASE_URL!,
+    USERS: {
+      ADMIN: {
+        username: process.env.LIVE_ADMIN_USERNAME!,
+        password: process.env.LIVE_ADMIN_PASSWORD!
+      },
+      USER: {
+        username: process.env.LIVE_USER_USERNAME!,
+        password: process.env.LIVE_USER_PASSWORD!
+      },
+      SUPPORT: {
+        username: process.env.LIVE_SUPPORT_USERNAME!,
+        password: process.env.LIVE_SUPPORT_PASSWORD!
+      }
     }
   }
 };
+
+export const ENV = ENV_CONFIG[CURRENT_ENV];
