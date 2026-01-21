b188e5693760612e56efe2e2cad14b2b64585aee
Mitesh Ahir <miteshabbtsource@outlook.com>
2026-01-21 18:23:59 +0530

    Test 15

--- tests/dashboard/dashboard.spec.ts
+++ tests/dashboard/dashboard.spec.ts
@@ -21,10 +21,10 @@ test.describe('Dashboard', () => {
     { tag: ['@smoke', '@regression'] },
     async ({ page }) => {
       // User is already logged in via storageState
-      await page.goto('/dashboard');
+      await page.goto('/dashboard1');
 
       const dashboard = new DashboardPage(page);
-      await page.waitForTimeout(5000);
+     
       // Verify balance section is visible
       await dashboard.verifyBalanceIsVisible();
     }
